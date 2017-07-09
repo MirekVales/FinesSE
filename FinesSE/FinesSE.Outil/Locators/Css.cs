ab8bfe3b4f4e09004af42624eaf962844b41ab60
Mirek Vales <MirekVales@users.noreply.github.com>
2017-07-10 00:26:35 +0200

    Css locator

---
+++ FinesSE/FinesSE.Outil/Locators/Css.cs
@@ -0,0 +1,18 @@
+﻿using FinesSE.Contracts.Infrastructure;
+using FinesSE.Contracts.Invokable;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+
+namespace FinesSE.Outil.Locators
+{
+    public class Css : ILocator
+    {
+        public IWebDriverProvider DriverProvider { get; set; }
+
+        public string Regex
+            => "(css=)(.+)";
+
+        public IEnumerable<IWebElement> Locate(string value)
+            => DriverProvider.Get().FindElements(By.CssSelector(value));
+    }
+}
