7647f9e0d8aecc4b7a4f38dc5128559c1864c808
Mirek Vales <MirekVales@users.noreply.github.com>
2017-08-26 10:58:54 +0200

    Safari driver support

---
+++ FinesSE/FinesSE.Drivers/Safari.cs
@@ -0,0 +1,33 @@
+﻿using FinesSE.Contracts;
+using FinesSE.Contracts.Infrastructure;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Safari;
+
+namespace FinesSE.Drivers
+{
+    public class Safari : IWebDriverActivator
+    {
+        public WebDrivers Id
+            => WebDrivers.Safari;
+
+        public IWebDriver Activate(IConfigurationProvider provider)
+        {
+            var configuration = provider.Get<SafariConfiguration>(null);
+            if (configuration == null)
+                return new SafariDriver();
+
+            return new SafariDriver(
+                configuration.WebDriverDirectory,
+                GetOptions(configuration),
+                configuration.CommandTimeout);
+        }
+
+        public SafariOptions GetOptions(SafariConfiguration configuration)
+        {
+            var options = new SafariOptions();
+            configuration.SetAdditionalCapability(options);
+
+            return options;
+        }
+    }
+}
---
+++ FinesSE/FinesSE.Drivers/SafariConfiguration.cs
@@ -0,0 +1,8 @@
+﻿using FinesSE.Contracts;
+
+namespace FinesSE.Drivers
+{
+    public class SafariConfiguration : WebDriverConfiguration
+    {
+    }
+}
