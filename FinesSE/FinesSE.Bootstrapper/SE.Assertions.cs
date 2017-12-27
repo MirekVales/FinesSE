240cf6d62db08aa2913a15f68f194d9ab3d7c50d
Mirek Vales <MirekVales@users.noreply.github.com>
2017-12-27 15:50:54 +0100

    VerifyEquality assertion

--- FinesSE/FinesSE.Bootstrapper/SE.Assertions.cs
+++ FinesSE/FinesSE.Bootstrapper/SE.Assertions.cs
@@ -4,6 +4,9 @@ namespace FinesSE.Bootstrapper
 {
     public partial class SE
     {
+        public void VerifyEquality(string expected, string actual)
+            => p.InvokeVoid<VerifyEquality>(expected, actual);
+
         public void VerifyText(string locator, string pattern)
             => p.InvokeVoid<VerifyText>(locator, pattern);
     }
