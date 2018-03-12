aa6b1db4d5d3ff4ff9146629797fbbb347c8c93f
Mirek Vales <MirekVales@users.noreply.github.com>
2018-03-12 22:14:34 +0100

    Acceptance tests update

---
+++ FinesSE/FinesSE.AcceptanceTests/WebDriverTest.cs
@@ -0,0 +1,56 @@
+﻿using FinesSE.Bootstrapper;
+using FinesSE.Contracts.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace FinesSE.AcceptanceTests
+{
+    [TestClass]
+    public class WebDriverTest
+    {
+        private SE se;
+
+        public string TestSiteUrl =>
+            @"file://" + Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                @"TestSites\1\index.html");
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            se = new SE();
+        }
+
+        [TestMethod]
+        [DataRow("css=.css_button_1", "Lorem ipsum", "1", true)]
+        [DataRow("css=.css_button_1", "Lorem ipsum", "10000", false)]
+        public void PageLoadCanTimeout(
+            string locator,
+            string expectedText,
+            string timeout,
+            bool fires)
+        {
+            se.SetPageLoadTimeout(timeout);
+            if (fires)
+                Assert.ThrowsException<ActionException>(
+                    () =>
+                    {
+                        se.Open(TestSiteUrl);
+                        se.VerifyText(locator, expectedText);
+                    });
+            else
+            {
+                se.Open(TestSiteUrl);
+                se.VerifyText(locator, expectedText);
+            }
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            se.Close();
+            se.Dispose();
+        }
+    }
+}
